cec5f6c8dd112150c83b665ed51a550ab5348001
kartick17 <karticksadhu0@gmail.com>
2024-09-06 22:48:01 +0530

    configure supabase

---
+++ src/services/apiCabins.js
@@ -0,0 +1,5 @@
+import supabase from './supabase'
+
+export async function getCabins() {
+  let { data: cabins, error } = await supabase.from('cabins').select('*')
+}
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://uovfzmfbvgoqyygbassf.supabase.co'
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvdmZ6bWZidmdvcXl5Z2Jhc3NmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0NjkyNzIsImV4cCI6MjA0MTA0NTI3Mn0.-BrjA9j84iSoy-P6N4T5dEoe5l3X2KM_S0aFE8nluso'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
