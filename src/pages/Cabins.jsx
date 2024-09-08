214b4e579b55f5e30d6d96a515d7d74aef82a385
kartick17 <karticksadhu0@gmail.com>
2024-09-08 21:26:26 +0530

    create cabin using react hook form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,17 +1,27 @@
+import { useState } from 'react'
 import CabinTable from '../features/cabins/CabinTable'
+import Button from '../ui/Button'
 import Heading from '../ui/Heading'
 import Row from '../ui/Row'
+import CreateCabinForm from '../features/cabins/CreateCabinForm'
 
 function Cabins() {
+  const [showCabin, setShowCabin] = useState(false)
+
   return (
     <>
-      <Row type="horizontal">
-        <Heading as="h1">All cabins</Heading>
+      <Row type='horizontal'>
+        <Heading as='h1'>All cabins</Heading>
         <p>Filter / Sort</p>
       </Row>
 
-      <div className="flex flex-col">
+      <div className='flex flex-col gap-14'>
         <CabinTable />
+        <Button onClick={() => setShowCabin((show) => !show)}>
+          Add new cabin
+        </Button>
+
+        {showCabin && <CreateCabinForm />}
       </div>
     </>
   )
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,6 +11,17 @@ export async function getCabins() {
   return cabins
 }
 
+export async function createCabin(newCabin) {
+  let { data: cabin, error } = await supabase.from('cabins').insert(newCabin)
+
+  if (error) {
+    console.error(error)
+    throw new Error('Cabin could not be created.')
+  }
+
+  return cabin
+}
+
 export async function deleteCabin(id) {
   let { data, error } = await supabase.from('cabins').delete().eq('id', id)
 
