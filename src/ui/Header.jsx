429c953be5971e57bd729708aacb4f02ff036508
kartick17 <karticksadhu0@gmail.com>
2024-09-09 01:28:06 +0530

    handling form errors

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
   return (
-    <header className="border-b border-gray-100 px-[4.8rem] py-[1.2rem]">
+    <header className='border-b border-gray-100 px-[4.8rem] py-[1.2rem]'>
       HEADER
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
